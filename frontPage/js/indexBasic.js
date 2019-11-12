2a6d05e67f04242636ecba60a3c42052f27f3800
Pudding Add pudding <hu1song2tao3@gmail.com>
2019-11-12 22:43:27 +0800

    前端主页面构建

---
+++ frontPage/js/indexBasic.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    //日期
+    var date = new Date();
+    var year=date.getFullYear();
+    var month=date.getMonth();
+    var day=date.getDate();
+    $("#currentDate").val(year+"-"+month+"-"+day);
+    
+    //开始|结束时间
+    var hour = date.getHours();
+    var min = date.getMinutes();
+    $("#startTime").val(hour+":"+min);
+    $("#finishTime").val(hour+":");
+
+  });
