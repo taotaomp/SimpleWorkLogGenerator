e64e52c468a9d688815ab32efd9ed857764d0b35
Pudding Add pudding <hu1song2tao3@gmail.com>
2019-11-18 11:12:07 +0800

    完成ajax到SpringMVC的连接，解决跨域问题

--- frontPage/js/indexAjax.js
+++ frontPage/js/indexAjax.js
@@ -1,9 +1,32 @@
 $(document).ready(function(){
-	$.getJSON("url",function(result){
-		$.each(result,function(i,filed){
+	// alert("d");
+	// document.cookie = "SameSite=None";
+	// $("#workType").click(function(){
+	// 	$.ajax({
+	// 		async : true,
+	// 		url : "http://localhost:8080/SimpleWorkLogGenerator_war/workTypes?callback=?",
+	// 		type : "GET",
+	// 		dataType : "jsonp", // 返回的数据类型，设置为JSONP方式
+	// 		jsonp : 'callback', //指定一个查询参数名称来覆盖默认的 jsonp 回调参数名 callback
+	// 		jsonpCallback: 'handleResponse', //设置回调函数名
+	// 		success: function(response, status, xhr){
+	// 		    console.log('状态为：' + status + ',状态是：' + xhr.statusText);
+	// 		    console.log(response);
+	// 			var jsondata = response;
+	// 			    console.info(jsondata);
+	// 			    console.info("姓名:"+jsondata[0].workTypeId);
+	// 			    console.info("年龄:"+jsondata.workTypeName);
+				    
+			
+	// 		}
+	// 	});
+	// });
+	
+	$.getJSON("http://localhost:8080/SimpleWorkLogGenerator_war/workTypes?callback=?",function(result){
+			$.each(result,function(i,filed){
 			var newOption = $("<option></option>");
-			newOption.attr("value",i);
-			newOption.append(filed);
+			newOption.attr("value",filed.workTypeId);
+			newOption.append(filed.workTypeName);
 			$("#workType").append(newOption);
 		});
 	});
--- src/main/java/cn/PApudding/SimpleWorkLogGenerator/web/GeneratorController.java
+++ src/main/java/cn/PApudding/SimpleWorkLogGenerator/web/GeneratorController.java
@@ -2,23 +2,31 @@ package cn.PApudding.SimpleWorkLogGenerator.web;
 
 import cn.PApudding.SimpleWorkLogGenerator.pojo.WorkType;
 import cn.PApudding.SimpleWorkLogGenerator.service.GeneratorService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.converter.json.MappingJacksonValue;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin
 public class GeneratorController {
 
     @Autowired
     private GeneratorService generatorService;
 
-    @RequestMapping(value = "/workTypes",method = RequestMethod.GET)
-    public List<WorkType> workTypeList(){
-        return generatorService.listWorkType();
+    @RequestMapping(value = "/workTypes",method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
+    public Object workTypeList(@RequestParam String callback) throws JsonProcessingException {
+        //MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(generatorService.listWorkType());
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        String temp = objectMapper.writeValueAsString(generatorService.listWorkType());
+        
+        return callback+"("+temp+")";
+
     }
 
     @RequestMapping(value = "/workType/{id}",method = RequestMethod.GET)
