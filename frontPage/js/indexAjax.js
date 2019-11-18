b914daed845d34baad9fb7bdc8c9a599e26774d9
Pudding Add pudding <hu1song2tao3@gmail.com>
2019-11-18 14:27:19 +0800

    优化SpringMVC去ajax跨域问题的解决方式

--- frontPage/js/indexAjax.js
+++ frontPage/js/indexAjax.js
@@ -1,27 +1,4 @@
 $(document).ready(function(){
-	// alert("d");
-	// document.cookie = "SameSite=None";
-	// $("#workType").click(function(){
-	// 	$.ajax({
-	// 		async : true,
-	// 		url : "http://localhost:8080/SimpleWorkLogGenerator_war/workTypes?callback=?",
-	// 		type : "GET",
-	// 		dataType : "jsonp", // 返回的数据类型，设置为JSONP方式
-	// 		jsonp : 'callback', //指定一个查询参数名称来覆盖默认的 jsonp 回调参数名 callback
-	// 		jsonpCallback: 'handleResponse', //设置回调函数名
-	// 		success: function(response, status, xhr){
-	// 		    console.log('状态为：' + status + ',状态是：' + xhr.statusText);
-	// 		    console.log(response);
-	// 			var jsondata = response;
-	// 			    console.info(jsondata);
-	// 			    console.info("姓名:"+jsondata[0].workTypeId);
-	// 			    console.info("年龄:"+jsondata.workTypeName);
-				    
-			
-	// 		}
-	// 	});
-	// });
-	
 	$.getJSON("http://localhost:8080/SimpleWorkLogGenerator_war/workTypes?callback=?",function(result){
 			$.each(result,function(i,filed){
 			var newOption = $("<option></option>");
--- src/main/java/cn/PApudding/SimpleWorkLogGenerator/web/GeneratorController.java
+++ src/main/java/cn/PApudding/SimpleWorkLogGenerator/web/GeneratorController.java
@@ -4,6 +4,7 @@ import cn.PApudding.SimpleWorkLogGenerator.pojo.WorkType;
 import cn.PApudding.SimpleWorkLogGenerator.service.GeneratorService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
@@ -18,15 +19,24 @@ public class GeneratorController {
     @Autowired
     private GeneratorService generatorService;
 
-    @RequestMapping(value = "/workTypes",method = RequestMethod.GET, produces = "text/html;charset=UTF-8")
-    public Object workTypeList(@RequestParam String callback) throws JsonProcessingException {
-        //MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(generatorService.listWorkType());
-        ObjectMapper objectMapper = new ObjectMapper();
+    /**
+     * 获取全部的WorkType
+     * @param callback json跨域问题的解决方案，设置返回方法名
+     * @return json对象
+     * @throws JsonProcessingException
+     */
+    @RequestMapping(value = "/workTypes",method = RequestMethod.GET/*, produces = "text/html;charset=UTF-8"*/)
+    public Object workTypeList(@RequestParam(value = "callback",required = false) String callback) throws JsonProcessingException {
+        List<WorkType> workTypes = generatorService.listWorkType();
+        if (callback==null){
+            return workTypes;
+        }
+        JSONPObject jsonpObject = new JSONPObject(callback,workTypes);
+        return jsonpObject;
 
-        String temp = objectMapper.writeValueAsString(generatorService.listWorkType());
-        
-        return callback+"("+temp+")";
-
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String temp = objectMapper.writeValueAsString(generatorService.listWorkType());
+//        return callback+"("+temp+")";
     }
 
     @RequestMapping(value = "/workType/{id}",method = RequestMethod.GET)
