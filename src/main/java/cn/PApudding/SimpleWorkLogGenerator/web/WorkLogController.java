74410d5902d9c1477e397600747fd076f1904a67
Pudding Add pudding <hu1song2tao3@gmail.com>
2019-11-26 14:30:57 +0800

    解决ajaxPOST跨域问题

--- src/main/java/cn/PApudding/SimpleWorkLogGenerator/web/WorkLogController.java
+++ src/main/java/cn/PApudding/SimpleWorkLogGenerator/web/WorkLogController.java
@@ -2,19 +2,22 @@ package cn.PApudding.SimpleWorkLogGenerator.web;
 
 import cn.PApudding.SimpleWorkLogGenerator.pojo.WorkLog;
 import cn.PApudding.SimpleWorkLogGenerator.service.WorkLogService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import com.fasterxml.jackson.databind.util.JSONPObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
-@CrossOrigin
+@CrossOrigin(value = "*",maxAge = 3000)
 public class WorkLogController {
     @Autowired
     private WorkLogService workLogService;
 
     @RequestMapping(value = "/workLog",method = RequestMethod.POST)
-    public Object postWorkLog(@RequestBody WorkLog workLog){
-        //JSONPObject jsonpObject = new JSONPObject();
+    public Object postWorkLog(@RequestBody WorkLog workLog , HttpServletResponse response){
         return workLogService.postWorkLog(workLog);
     }
 }
